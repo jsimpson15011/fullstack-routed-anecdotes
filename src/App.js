e1103965aac0012bb75c2a25000351b85c9203a8
Joseph Simpson <joseph.simpson@thehomeslicegroup.com>
2019-11-01 11:15:01 -0600

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
+import {
+  BrowserRouter as Router,
+  Route, Link, Redirect, withRouter
+} from "react-router-dom"
 
 const Menu = () => {
   const padding = {
@@ -6,19 +10,36 @@ const Menu = () => {
   }
   return (
     <div>
-      <a href='#' style={padding}>anecdotes</a>
-      <a href='#' style={padding}>create new</a>
-      <a href='#' style={padding}>about</a>
+      <Link style={padding} to="/">anecdotes</Link>
+      <Link style={padding} to="/create-new">create new</Link>
+      <Link style={padding} to="/about">about</Link>
     </div>
   )
 }
 
-const AnecdoteList = ({ anecdotes }) => (
+const Notification = ({notification}) => (
   <div>
+    {notification}
+  </div>
+)
+
+const AnecdoteList = ({anecdotes, notification}) => {
+  return (
+  <div>
+    {notification ? <Notification notification={notification}/> : ''}
     <h2>Anecdotes</h2>
     <ul>
-      {anecdotes.map(anecdote => <li key={anecdote.id} >{anecdote.content}</li>)}
+      {anecdotes.map(anecdote => <li key={anecdote.id}><Link to={`/anecdote/${anecdote.id}`} >{anecdote.content}</Link></li>)}
     </ul>
+  </div>
+)}
+
+const Anecdote = ({anecdote}) => (
+  <div>
+    <h2>
+      {anecdote.content}
+    </h2>
+    has {anecdote.votes} votes
   </div>
 )
 
@@ -28,8 +49,10 @@ const About = () => (
     <p>According to Wikipedia:</p>
 
     <em>An anecdote is a brief, revealing account of an individual person or an incident.
-      Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself,
-      such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative.
+      Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter
+      but to reveal a truth more general than the brief tale itself,
+      such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a
+      person, place, or thing through the concrete details of a short narrative.
       An anecdote is "a story with a point."</em>
 
     <p>Software engineering is full of excellent anecdotes, at this app you can find the best and add more.</p>
@@ -40,15 +63,16 @@ const Footer = () => (
   <div>
     Anecdote app for <a href='https://courses.helsinki.fi/fi/tkt21009'>Full Stack -sovelluskehitys</a>.
 
-    See <a href='https://github.com/fullstack-hy2019/routed-anecdotes/blob/master/src/App.js'>https://github.com/fullstack-hy2019/routed-anecdotes/blob/master/src/App.js</a> for the source code.
+    See <a
+    href='https://github.com/fullstack-hy2019/routed-anecdotes/blob/master/src/App.js'>https://github.com/fullstack-hy2019/routed-anecdotes/blob/master/src/App.js</a> for
+    the source code.
   </div>
 )
 
-const CreateNew = (props) => {
+const CreateNewWithNoHistory = (props) => {
   const [content, setContent] = useState('')
   const [author, setAuthor] = useState('')
   const [info, setInfo] = useState('')
-
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -58,6 +82,9 @@ const CreateNew = (props) => {
       info,
       votes: 0
     })
+    props.setNotification(`a new anecdeote ${content} created!`)
+    setTimeout(()=>props.setNotification(''),10000)
+    props.history.push('/')
   }
 
   return (
@@ -66,22 +93,23 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input name='content' value={content} onChange={(e) => setContent(e.target.value)}/>
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)}/>
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e) => setInfo(e.target.value)} />
+          <input name='info' value={info} onChange={(e) => setInfo(e.target.value)}/>
         </div>
         <button>create</button>
       </form>
     </div>
   )
-
 }
+
+const CreateNew = withRouter(CreateNewWithNoHistory)
 
 const App = () => {
   const [anecdotes, setAnecdotes] = useState([
@@ -123,15 +151,20 @@ const App = () => {
   }
 
   return (
-    <div>
-      <h1>Software anecdotes</h1>
-      <Menu />
-      <AnecdoteList anecdotes={anecdotes} />
-      <About />
-      <CreateNew addNew={addNew} />
-      <Footer />
-    </div>
+    <Router>
+      <div>
+        <h1>Software anecdotes</h1>
+        <Menu/>
+        <Route exact path="/" render={() => <AnecdoteList anecdotes={anecdotes} notification={notification}/>}/>
+        <Route exact path="/anecdote/:id" render={({ match }) =>
+          <Anecdote anecdote={anecdoteById(match.params.id)} />
+        } />
+        <Route path="/create-new" render={() => <CreateNew addNew={addNew} setNotification={setNotification}/>}/>
+        <Route path="/about" render={() => <About/>}/>
+        <Footer/>
+      </div>
+    </Router>
   )
 }
 
-export default App;
+export default App
